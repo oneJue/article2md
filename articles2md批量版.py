7bfad60e005946f7d5318523bd34f410d419ec6b
Jue <103429373+oneJue@users.noreply.github.com>
2023-04-25 17:27:15 +0800

    Update articles2md批量版.py

--- articles2md批量版.py
+++ articles2md批量版.py
@@ -1,30 +1,9 @@
-
-from html2text import HTML2Text
-from bs4 import BeautifulSoup
 from lxml import etree
+from bs4 import BeautifulSoup
 import requests
 import os
 import re
 
-
-def get_article_url(html_1):   #获取分类专栏内的文章title和url
-    soup=BeautifulSoup(html_1,'lxml')
-    urlarray=[]
-    print("该分类中的专栏文章")
-    for i in soup.select('.column_article_list li'):
-        title = re.sub('[\/:*?"<>|]','-',i.find('h2').text).strip()
-        url=i.find('a')['href']
-        print("title:",title)
-        print("url:",url)
-        print("----------------------------------------------------------------")
-        urlarray.append(url)
-    return urlarray
-
-
-def remove(html): #去除文章多余内容(乱七八糟的东西“
-    soup = BeautifulSoup(html, 'lxml')  # 传入解析器：lxml
-    html = soup.select('article')
-    return html
 
 def get_html(url):
     headers = {
@@ -40,45 +19,65 @@ def get_html(url):
         print("failed!")
     return html
 
+def get_article_url(html_1):   #获取分类专栏内的文章title和url
+    soup=BeautifulSoup(html_1,'lxml')
+    urlarray=[]
+    print("该分类中的专栏文章")
+    for i in soup.select('.column_article_list li'):
+        title = re.sub('[\/:*?"<>|]','-',i.find('h2').text).strip()
+        url=i.find('a')['href']
+        print("title:",title)
+        print("url:",url)
+        print("----------------------------------------------------------------")
+        urlarray.append(url)
+    return urlarray
 
+def save(html,md):
+    tree = etree.HTML(html)
+    # 找到需要的html块
+    title = re.sub('[\/:*?"<>|]', '-', tree.xpath('//*[@id="articleContentId"]/text()')[0])
+    with open(f"output/{title}.md", 'w', encoding='utf8') as md_file:
+        md_file.write(md)
 
-def crawl(html):
-        tree = etree.HTML(html)
-        # 找到需要的html块
-        title = re.sub('[\/:*?"<>|]', '-', tree.xpath('//*[@id="articleContentId"]/text()')[0])
-        print("title:", title)
-        block = remove(html)
-        save(block, title)
-        print("finish!")
-        # 完成！
+def crawl(objurl):
+    url = "https://devtool.tech/api/html-md"
+    # 请求头
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
+    }
+    # body数据
 
+    data = {"url": objurl}
+    # 发送请求
+    r = requests.post(url, headers=headers, data=data)
+    # 判断是否登录成功
+    if r.text is not None:
+        print("get markdown success!")
+    else:
+        print("failed!")
 
+    html = get_html(objurl)
+    md = eval(r.text)['markdown']
+    save(html, md)
 
-def save(block, title):
-    if "output" not in os.listdir():
-        # 不存在输出文件夹就创建
-        os.mkdir("output")
-        os.mkdir("output/markdown")
-
-    with open(f"output/markdown/{title}.md", 'w', encoding='utf8') as md_file:
-        # 保存markdown
-
-        text_maker = HTML2Text()
-        # md转换
-        md_text = text_maker.handle(str(block[0]))
-        md_file.write(md_text)
 
 
 if __name__ == '__main__':
+    if "output" not in os.listdir():
+        # 不存在输出文件夹就创建
+        os.mkdir("output")
+
     choice =input("输入1爬取单篇文章，输入2爬取分类专栏:")
     if choice == "1":
     #单篇文章# 你想要爬取的文章url
-        url = input("输入目标url:")
-        crawl(get_html(url))
+        objurl = input("输入目标url:")
+        crawl(objurl)
     if choice == "2":
     #分类专栏# 你想要爬取的文章url
         url = input("输入目标url:")
         for url in get_article_url(get_html(url)):
-            crawl(get_html(url))
+            crawl(url)
     else:
         print("input error")
+
+
